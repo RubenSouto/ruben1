17fb97f6c25e427ecaa8f9019a319dc3df335d48
RubenSouto <61864061+RubenSouto@users.noreply.github.com>
2020-10-28 15:50:38 +0100

    Funktioniert 80%

--- SiebJava/app.js
+++ SiebJava/app.js
@@ -1,48 +1,43 @@
-
+var waitVar;
 
 /**
  * Erstellt Array mit Zahlen (0 -max)
  * Alle Primzahlen kommen in den Array: primZahlen
  * dieser wird zurück gegeben
- * @param {number} max 
  */
-function Sieb(max) {
+function Sieb() {
     var numbers = [];
     var primZahlen = [];
-
+    var max = document.getElementById("userInput").value;
+    
     for(var i= 2; i <= max; i++){
         numbers.push(i);
     }
 
-    //document.getElementById("zahlen").innerHTML = numbers;
+    document.getElementById("zahlen").innerHTML = numbers;
 
     while (numbers.length > 0) {
         primZahlen.push(numbers.shift());
 
-        document.getElementById("zahlen").innerHTML = "";
-        document.getElementById("primZahlen").innerHTML = "";    
+        setTimeout(plsWait(primZahlen), 3000);
 
-        setTimeout(() => {
-            document.getElementById("primZahlen").innerHTML = primZahlen;
-            document.getElementById("zahlen").innerHTML = numbers;
-        }, 3000);
-
-        setTimeout(() => {
-            for (var i = 0; i <= numbers.length; i++){
-                if (numbers[i] % primZahlen[primZahlen.length - 1] == 0){
-                    numbers.splice(i, 1);
-                }
+        for (var i = 0; i <= numbers.length; i++){
+            if (numbers[i] % primZahlen[primZahlen.length - 1] == 0){
+                numbers.splice(i, 1);
             }
-        }, 3000);
+        }
     }
-    //document.getElementById("primZahlen").innerHTML = primZahlen;
     return primZahlen;
 }
-console.log(Sieb(100));
 
+function createTable(numbers){
 
-function KastenErstellen(){
-    var max = document.getElementById("userInput").value;
- 
-    console.log(Sieb(max));
+}
+
+function plsWait(primZahlen) {
+    setTimeout(printArrays(primZahlen), 3000);
+}
+
+function printArrays(primZahlen) {
+    document.getElementById("primZahlen").innerHTML = primZahlen;
 }
